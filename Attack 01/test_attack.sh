1369b5dfeda614b554149c76cc48019463f0987c
Wania Shafqat <73712563+waniashafqat@users.noreply.github.com>
2024-07-03 22:36:59 +0500

    Delete Attack 01 directory

--- Attack 01/test_attack.sh
+++
@@ -1,8 +0,0 @@
-# LO Norm
-python ./attack.py --sourceImg 70.png --targetImg turn.jpg --outputImg outputl0.jpg --attackImg attackl0.jpg --resizeFunc cv2.resize --interpolation cv2.INTER_LINEAR --penalty 0.1 --imageFactor 255 --norm l0
-
-# L2 Norm
-python ./attack.py --sourceImg 70.png --targetImg turn.jpg --outputImg outputl2.jpg --attackImg attackl2.jpg --resizeFunc cv2.resize --interpolation cv2.INTER_LINEAR --penalty 0.1 --imageFactor 255 --norm l2
-
-# LI Norm
-#python ./attack.py --sourceImg 70.png --targetImg turn.jpg --outputImg outputli.jpg --attackImg attackli.jpg --resizeFunc cv2.resize --interpolation cv2.INTER_LINEAR --penalty 0.01 --imageFactor 255 --norm li
